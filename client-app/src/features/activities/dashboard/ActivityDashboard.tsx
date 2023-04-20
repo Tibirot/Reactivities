866ede92e83173e1588df160c2e12b14e768dbe1
Stiberiu <tiberiu.socea@nestorup.com>
2023-04-20 17:34:20 +0300

    added local CRUD operations on client side

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,40 @@
+import { Grid } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
+import ActivityDetails from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
+
+interface Props {
+    activities: Activity[];
+    selectedActivity: Activity | undefined;
+    editMode: boolean;
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    openForm: (id: string) => void;
+    closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
+}
+
+const ActivitiesDashboard = ({ 
+    activities,  selectedActivity,  selectActivity, createOrEdit, 
+    cancelSelectActivity, editMode, openForm, closeForm, deleteActivity}: Props) => {
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {selectedActivity && 
+                <ActivityDetails 
+                activity={selectedActivity} 
+                cancelSelectActivity={cancelSelectActivity}
+                openForm={openForm} />}
+                {editMode &&
+                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+            </Grid.Column>
+        </Grid>
+    )
+}
+
+export default ActivitiesDashboard;
